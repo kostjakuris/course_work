a22c47c880b21863c34438a77af4268ae39995e9
Kostjn04 <kostjn04e@gmail.com>
2023-11-30 18:51:43 +0200

    completed index.js

--- node_express/index.js
+++ node_express/index.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const cors = require("cors");
 const Post = require("./Post.js");
 
 require("dotenv").config();
 
-// const DB_URL = `mongodb+srv://kostjn04e:user@cluster0.rn2vxp0.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 
-// const corsOptions = {
-//     origin: "https://course-work-rosy.vercel.app/",
-//     optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
 app.post("/", async (req, res) => {
     try {
         const {email} = req.body;
