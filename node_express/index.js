da3b2864ef9492117896559ebbbb279d59601a00
Kostjn04 <kostjn04e@gmail.com>
2023-11-30 17:27:32 +0200

    some trying in index.js

--- node_express/index.js
+++ node_express/index.js
@@ -1,41 +1,51 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// // const cors = require("cors");
+// const Post = require("./Post");
+//
+//
+// const DB_URL = `mongodb+srv://kostjn04e:user@cluster0.rn2vxp0.mongodb.net/`;
+// const PORT = 5000;
+//
+// const app = express();
+// // require("dotenv").config();
+// app.use(express.json());
+//
+// // const corsOptions = {
+// //     origin: "https://course-work-rosy.vercel.app/",
+// //     optionSuccessStatus: 200,
+// // };
+// // app.use(cors(corsOptions));
+// app.post("/", async (req, res) => {
+//     try {
+//         const {email} = req.body;
+//         await Post.create({email});
+//         res.status(200).json("Subscribe successful");
+//     } catch (e) {
+//         res.status(500).json(e);
+//     }
+// });
+//
+// async function startApp() {
+//     try {
+//         await mongoose.connect(DB_URL);
+//         console.log("Connection successful");
+//         app.listen(PORT, () => console.log("Server started on port " + PORT));
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+//
+// startApp();
+//
+// module.exports = app;
+
 const express = require("express");
-const mongoose = require("mongoose");
-// const cors = require("cors");
-const Post = require("./Post");
-
-
-const DB_URL = `mongodb+srv://kostjn04e:user@cluster0.rn2vxp0.mongodb.net/`;
-const PORT = 5000;
 
 const app = express();
-// require("dotenv").config();
-app.use(express.json());
 
-// const corsOptions = {
-//     origin: "https://course-work-rosy.vercel.app/",
-//     optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-app.post("/", async (req, res) => {
-    try {
-        const {email} = req.body;
-        await Post.create({email});
-        res.status(200).json("Subscribe successful");
-    } catch (e) {
-        res.status(500).json(e);
-    }
+app.use("/", (req, res) => {
+    res.send("server is running");
 });
 
-async function startApp() {
-    try {
-        await mongoose.connect(DB_URL);
-        console.log("Connection successful");
-        app.listen(PORT, () => console.log("Server started on port " + PORT));
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-startApp();
-
-module.exports = app;
+app.listen(5000, console.log("server started in port 5000"));
